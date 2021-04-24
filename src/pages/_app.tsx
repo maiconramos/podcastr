64562f8a1faa48dc3caf031600ddd5bbbdf7ff45
maiconramos <maiconx@outlook.com>
2021-04-24 14:00:37 -0300

    finish aula 5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,31 +5,12 @@ import { Player } from '../components/Player'
 
 import styles from '../styles/app.module.scss'
 import Home from '.'
-import { PlayerContext } from '../contexts/PlayerContext'
-import { useState } from 'react'
+import { PlayerContextProvider } from '../contexts/PlayerContext'
 
 
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying , setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state){
-    setIsPlaying(state);
-  }
-
-  return (
-    <PlayerContext.Provider value={{ episodeList , currentEpisodeIndex , play , isPlaying , togglePlay , setPlayingState}}>
+    return (
+      <PlayerContextProvider>
         <div className={styles.wrapper}>
           <main>
             <Header />
@@ -37,7 +18,7 @@ function MyApp({ Component, pageProps }) {
           </main>
           <Player />
         </div>
-    </PlayerContext.Provider>
+      </PlayerContextProvider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import { GetStaticProps } from "next";
 import { api } from "../services/api";
-import { useContext } from "react";
+import Head from "next/head";
 import Image from 'next/image'; 
 import Link from 'next/link';
 import { format , parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 
 import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
-import { PlayerContext } from "../contexts/PlayerContext";
+import { PlayerContext, usePlayer } from "../contexts/PlayerContext";
 
 import styles from "../pages/home.module.scss";
 
@@ -29,15 +29,20 @@ type HomeProps = {
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
-  const { play } = useContext(PlayerContext);
+  const { playList } = usePlayer();
+
+  const episodeList = [...latestEpisodes, ...allEpisodes];
 
   return (
     <div className={styles.homepage}>
+      <Head>
+          <title>Home | Podcastr</title>
+      </Head>
       <section className={styles.latestEpisodes}>
         <h2>Últimos lançamentos</h2>
 
         <ul>
-          {latestEpisodes.map((episode) => {
+          {latestEpisodes.map((episode , index) => {
             return(
               <li key={episode.id}>
                 <Image 
@@ -56,7 +61,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button" onClick={() => play(episode)}>
+                <button type="button" onClick={() => playList(episodeList, index)}>
                   <img src="/play-green.svg" alt="Tocar episódio"/>
                 </button>
               </li>
@@ -81,7 +86,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
               </tr>
             </thead>
             <tbody>
-              {allEpisodes.map(episode => {
+              {allEpisodes.map((episode , index) => {
                 return(
                   <tr key={episode.id}>
                     <td style={{width: 72}}>
@@ -102,7 +107,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                     <td style={{width: 100}}>{episode.publishedAt}</td>
                     <td>{episode.durationAsString}</td>
                     <td>
-                      <button type="button" onClick={() => play(episode)} disabled={!episode}>
+                      <button type="button" onClick={() => playList(episodeList, index + latestEpisodes.length)} disabled={!episode}>
                         <img src="/play-green.svg" alt="Tocar episódio" />
                       </button>
                     </td>
